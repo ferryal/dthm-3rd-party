09f4693705e10fcbc1468fbeb861e8985832f769
Ferryal Fajar <ferryal21@gmail.com>
2025-04-12 20:58:27 +0700

    fix: adjust webpack to load style

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
